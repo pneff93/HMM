6168d01201cdb0666a9d31a1627f658b4b215ac2
MrLehna <malte.lehna@live.de>
2018-08-07 19:06:26 +0200

    Test function added

--- tests/testthat/test_HMM3.R
+++ tests/testthat/test_HMM3.R
@@ -1,4 +1,15 @@
 context("HMM3 Test control")
+source("Transformation.R")
 
-test_that("")
+test_that("Probit Control mit p=0.5", {
+  #Due to the fact that the probit transformation uses the exponential function
+  #we should get the probability 0.5 for Sigma and Gamma alike if we use m=2 
+  #and the factor c(0,0,0,1,1). Theta will be a vector of c(1,1)
+  
+  prob_test <- trans(c(0,0,0,1,1),m=2)
+  
+  expect_equal(prob_test[,1],c(0.5,0.5))
+  expect_equal(prob_test[,2:3],matrix(c(0.5,0.5,0.5,0.5),2,2))
+  expect_equal(prob_test[,4],c(1,1))
+})
 
