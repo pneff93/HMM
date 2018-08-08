968712cc1aedead3e35d6b0190795ea2bba016b9
MrLehna <malte.lehna@live.de>
2018-08-08 11:22:48 +0200

    Neue Test

--- tests/testthat/test_HMM2.R
+++ tests/testthat/test_HMM2.R
@@ -15,7 +15,7 @@ test_that("Testing the output of the alpha function", {
   expect_equal(alpha_test1[[1]],matrix (c(0.5,0.5,0.5,0.5),2,2))
   expect_equal(alpha_test1[[2]] ,c(1,1))
   
-  #Now for the fact that they are fully unlikely, we expect NaN due to the fact, that we devide through zeor 
+  #Now for the fact that they are fully unlikely, we expect NaN due to the fact, that we devide through zero 
   #for the weights the first sum is equal to zero (sums of zero prob.), and the second one should also be NaN
   p2 <- c(0,0,0,0)
   alpha_test2 <- alpha_function(m=2,N=2,sigma1,gamma1,p2,set1)
@@ -32,8 +32,31 @@ test_that("Output of Beta function",{
   gamma1 <- matrix (c(0.5,0.5,0.5,0.5),2,2) 
   p1 <- c(1,1,1,1)
   set1 <- c(1,3)
+  c1 <-c(1,1)
   
-  beta1_test <- beta_function(2,2,gamma1,p1,set1,c=c(1,1))
+  beta1_test <- beta_function(2,2,gamma1,p1,set1,c1)
   
   expect_equal(beta1_test,matrix(c(1,1,1,1),2,2))
 })
+
+test_that("Output u-function and v- function",{
+  #we set the previous alphas and betas and expect the output to be
+  #0.5,0.5
+  alpha1 <- matrix (c(0.5,0.5,0.5,0.5),2,2) 
+  beta1 <- matrix(c(1,1,1,1),2,2)
+  gamma1 <- matrix (c(0.5,0.5,0.5,0.5),2,2) 
+  c1<-c(1,1)
+  p1 <- c(1,1,1,1)
+  set1 <- c(1,3)
+  
+  u_test <- u_function(2,2,alpha1,beta1)
+  v_test <- v_function(2,2,beta1,p1,c1,gamma1,set1,u=u_test)
+  
+  
+  expect_equal(u_test,matrix (c(0.5,0.5,0.5,0.5),2,2) )
+  expect_equal(v_test,matrix(c(0.25,0.25,0.25,0.25),1,4))
+  })
+
+
+  
+  
--- tests/testthat/test_HMM3.R
+++ tests/testthat/test_HMM3.R
@@ -8,14 +8,14 @@ test_that("Probit Control mit p=0.5", {
   prob_test <- trans(c(0,0,0,1,1),m=2)
   
   expect_equal(prob_test[,1],c(0.5,0.5))
-  expect_equal(prob_test[,2] ,c(0.5,0.5))
-  expect_equal(prob_test[,3] ,c(0.5,0.5))
+  expect_equal(prob_test[,2],c(0.5,0.5))
+  expect_equal(prob_test[,3],c(0.5,0.5))
   expect_equal(prob_test[,4],c(1,1))
 })
 
 test_that("Control of Likelihood function", {
   #with a fixed set of parameters we expect to gain the same result for the likelihood
-  #every time. factor= c(0,0,0,1,1), L1=L2=normal distr. with sigma=0 and m=2 
+  #every time. factor= c(0,0,0,1,1), L1=L2=normal distr. with sd=1 and m=2 
   # x1 =c(1,1,0,0) and x2 =c(1,1,1,1)
   expect_equal(round(LH(c(0,0,0,1,1),m=2,x=c(1,1,0,0),L1=dnorm,L2=dnorm),4), 4.6758)
   expect_equal(round(LH(c(0,0,0,1,1),m=2,x=c(1,1,1,1),L1=dnorm,L2=dnorm),4), 3.6758)
