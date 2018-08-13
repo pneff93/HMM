edfc46d5266f561ef1f338b6288009dd539a596c
MrLehna <malte.lehna@live.de>
2018-08-13 17:58:59 +0200

    three state Multi added (definetly using for example, because results are awesome)
    Test for decoding added

---
+++ examples/multi_Norm_threeStates.R
@@ -0,0 +1,88 @@
+##RESULTS:
+#Both methods need some estimation time but estimate the parameters well
+
+
+#transition matrix
+gamma<-matrix(c(0.7,0.25,0.05,
+                0.1,0.4,0.5,
+                0.3,0.5,0.2), byrow=T, nrow=3)
+
+#initial state probabilities
+sigma<-c(0.5, 0.3, 0.2)
+
+#sample size
+n<- 500
+
+x<-c()
+set.seed(100)
+s1<-rnorm(10000, 7, 1)
+s2<-rnorm(10000, 2, 4)
+s3<-rnorm(10000, 15, 3)
+
+
+#initial state
+random_number<-runif(1, 0, 1)
+
+if (random_number < sigma[1]){
+  x[1]<-sample(s1, 1, replace = F)
+  p<-1
+} else if (random_number < sum(sigma[1:2]) && random_number > sigma[1]) {
+  x[1]<-sample(s2, 1, replace = F)
+  p<-2
+} else {
+  x[1]<-sample(s3, 1, replace = F)
+  p<-3
+}
+
+#sample creation
+for (i in 2:n){
+  random_number<-runif(1, 0, 1)
+  if (random_number < gamma[p,1]){
+    p<-1
+    x[i]<-sample(s1, 1, replace = F)
+  } else if(random_number < sum(gamma[p,1:2]) && random_number > gamma[p,1]) {
+    p<-2
+    x[i]<-sample(s2, 1, replace = F)
+  } else {
+    p<-3
+    x[i]<-sample(s3, 1, replace = F)
+  }
+}
+
+
+#likelihoods
+
+#the first likelihood only requires one parameter. The other two need two
+
+L1<-function(x, theta){
+  mu <- theta 
+  
+  p1<-1/sqrt(2*pi)*exp(-0.5*(x-mu)^2)
+  return(p1)
+}
+
+L2<-function(x,theta){
+  mu <- theta[1]
+  sd <- theta[2]
+  p2<-1/sqrt(2*pi*(sd^2))*exp(-((x-mu)^2)/(2*sd^2))
+  return(p2)
+}
+
+L3<-function(x,theta){
+  mu <- theta[1]
+  sd <- theta[2]
+  p3<-1/sqrt(2*pi*(sd^2))*exp(-((x-mu)^2)/(2*sd^2))
+  return(p3)
+}
+
+
+
+m<-3
+
+hist(x)
+#intial estimates of theta
+
+theta1 <- list(8,c(1,1),c(20,1))
+
+multifactorHMM(x=x,theta=theta1,m=m,method="EM",L1=L1,L2=L2,L3=L3,decoding = TRUE)
+multifactorHMM(x=x,theta=theta1,m=m,method="DM",L1=L1,L2=L2,L3=L3,decoding = TRUE)
---
+++ tests/testthat/test_Decoding.R
@@ -0,0 +1,27 @@
+context("Test of decoding")
+
+test_that("Test if the decoding estimates the right proberties",{
+  
+  z <- c(0,0,10,10,0,10)
+  
+  L1<-function(x, mu){
+    p1<-1/sqrt(2*pi)*exp(-0.5*(x-mu)^2)
+    return(p1)
+  }
+  
+  #Test input
+  gamma1 <- matrix (c(0.5,0.5,0.5,0.5),2,2)
+  sigma1 <- c(1,0)
+  theta1 <- c(0,10)
+  
+  #We expect both paths to return an path of 1,1,2,2,1,2, because with sigma c(1,0) we set the first theta as first path and the
+  #z falls exactly onto the estimated theta (thus highest probabilty )
+  #The decoding styles should not return different results 
+  
+  test_out <- decode(x=z, m=2, L1=L1, L2=L1, gamma= gamma1,sigma=sigma1,theta=theta1,multi=FALSE)
+    
+  expect_equal(test_out$Local_Decoding,c(1,1,2,2,1,2))
+  expect_equal(test_out$Global_Decoding,c(1,1,2,2,1,2))
+  expect_equal(test_out$Local_Decoding,test_out$Global_Decoding)
+  
+  })
